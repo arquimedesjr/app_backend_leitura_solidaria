16535f3766bb5e106612fff5dd6cea96a279bc6e
Arquimedes Junior <arquimedeslqjunior@gmail.com>
2021-03-01 20:47:04 -0300

    adicionando metodo save e find no Service

--- src/main/java/br/com/backend/leitura_solidaria/services/UsersService.java
+++ src/main/java/br/com/backend/leitura_solidaria/services/UsersService.java
@@ -2,9 +2,11 @@ package br.com.backend.leitura_solidaria.services;
 
 import br.com.backend.leitura_solidaria.domain.Users;
 import br.com.backend.leitura_solidaria.repositories.UsersRepository;
+import br.com.backend.leitura_solidaria.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -13,8 +15,18 @@ public class UsersService {
     @Autowired
     private UsersRepository repo;
 
-    public Users search(Integer id){
-        Optional<Users> obj =  repo.findById(id);
-        return obj.orElse(null);
+    public List<Users> search(){
+        return repo.findAll();
     }
+
+    public Users save(Users users) {
+        return repo.save(users);
+    }
+
+    public Users find(Integer id) {
+        Optional<Users> obj =  repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! Id: " + id + ", Tipo: " + Users.class.getName()));
+    }
+
 }
