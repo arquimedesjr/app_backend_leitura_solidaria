b97945d5c2b8f73c50709faf4fe5625d5e80b8a2
Arquimedes Junior <arquimedeslqjunior@gmail.com>
2021-03-01 20:45:55 -0300

    ajustando mapeamento principal para 'api' e adicionando POST e um GET para buscar todos os usuarios

--- src/main/java/br/com/backend/leitura_solidaria/resources/UsersResource.java
+++ src/main/java/br/com/backend/leitura_solidaria/resources/UsersResource.java
@@ -5,20 +5,35 @@ import br.com.backend.leitura_solidaria.services.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.util.ArrayList;
+import java.net.URI;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/users")
+@RequestMapping(value = "/api")
 public class UsersResource {
 
     @Autowired
     private UsersService service;
 
-    @RequestMapping(value = {"/{id}"}, method = RequestMethod.GET)
-    public ResponseEntity<?> listUsers(@PathVariable Integer id) {
-        Users obj = service.search(id);
+    @GetMapping(value = {"/users"})
+    public ResponseEntity<?> listUsers() {
+        List<Users> obj = service.search();
         return ResponseEntity.ok().body(obj);
+    }
+
+    @GetMapping(value = {"/users/{id}"})
+    public ResponseEntity<?> listUsers(@PathVariable Integer id) {
+        Users obj = service.find(id);
+        return ResponseEntity.ok().body(obj);
+    }
+
+    @PostMapping("/users")
+    public ResponseEntity<?> insertUsers(@RequestBody Users obj) {
+        obj = service.save(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(obj.getIdUsers()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
