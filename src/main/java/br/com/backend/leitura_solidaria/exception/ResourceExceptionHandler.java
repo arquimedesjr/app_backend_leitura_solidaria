c1545b1fa30c63642ebbdbb28357e4aacb608130
Arquimedes Junior <arquimedeslqjunior@gmail.com>
2021-06-05 13:48:14 -0300

    Add excepitons

--- src/main/java/br/com/backend/leitura_solidaria/exception/ResourceExceptionHandler.java
+++ src/main/java/br/com/backend/leitura_solidaria/exception/ResourceExceptionHandler.java
@@ -1,5 +1,6 @@
 package br.com.backend.leitura_solidaria.exception;
 
+import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -27,6 +28,12 @@ public class ResourceExceptionHandler {
     public ResponseEntity<StandarError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
         ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(), "Erro de validação", System.currentTimeMillis());
         e.getBindingResult().getFieldErrors().forEach(x-> err.addErro(x.getField(),x.getDefaultMessage()));
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+    }
+
+    @ExceptionHandler(InvalidDataAccessApiUsageException.class)
+    public ResponseEntity<StandarError> validation(InvalidDataAccessApiUsageException e, HttpServletRequest request) {
+        StandarError err = new StandarError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
     }
 }
